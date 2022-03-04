4e58742b1dfd68a12aaaac793c76a29de6dd7eef
msabeni <kemimsabeni@gmail.com>
2022-03-04 10:25:30 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>K_ART</title>
+    <!-- ==== Main CSS ====-->
+    <link rel="stylesheet" href="style.css" />
+    <!-- ===== Fonts =====-->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+      rel="stylesheet"
+    />
+    <!--font awesome-->
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+  </head>
+  <body>
+    <div class="header">
+      <!--==== Nav Bar Start ====-->
+      <div class="container">
+        <div class="navbar">
+          <div class="logo">
+            <img
+              src="images/art_logo2_ccexpress.png"
+              alt="logo"
+              width="200px"
+              height="200px"
+            />
+          </div>
+          <nav>
+            <ul>
+              <li><a href="">Home</a></li>
+              <li><a href="">Products</a></li>
+              <li><a href="">About</a></li>
+              <li><a href="">Contact</a></li>
+              <li><a href="">Account</a></li>
+            </ul>
+          </nav>
+          <img
+            src="images/cart_ccexpress.png"
+            alt="cart"
+            width="30px"
+            height="30px"
+          />
+        </div>
+        <!-- ==== End navbar ==== -->
+
+        <!-- ==== first section ==== -->
+        <div class="row">
+          <div class="col-2">
+            <h1>
+              Welcome to K_ART. <br />
+              Buy my art !
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              <br />
+              aliquam natus suscipit assumenda atque voluptatem !
+            </p>
+            <a href="" class="btn">Explore Now &#8594;</a>
+          </div>
+          <div class="col-2">
+            <img src="images/front_image1.jpg" alt="#" />
+          </div>
+        </div>
+        <!-- ==== end first section ==== -->
+      </div>
+    </div>
+
+    <!-- ==== featured categories ==== -->
+    <div class="categories">
+      <div class="small-container">
+        <div class="row">
+          <div class="col-3">
+            <img src="images/category-1.jpeg" alt="" />
+          </div>
+          <div class="col-3"><img src="images/category-2.jpeg" alt="" /></div>
+          <div class="col-3"><img src="images/category-3.jpeg" alt="" /></div>
+        </div>
+      </div>
+    </div>
+
+    <!-- ==== featured products ==== -->
+    <div class="small-container">
+      <h2 class="title">Featured products</h2>
+      <div class="row">
+        <div class="col-4">
+          <img src="images/product-1.jpeg" alt="" />
+          <h4>Abstract guitar</h4>
+          <div class="rating">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star-o"></i>
+          </div>
+          <p>$50.00</p>
+          <a href="" class="btn">Details &#8594;</a>
+        </div>
+        <div class="col-4">
+          <img src="images/product-2.jpeg" alt="" />
+          <h4>Abstract guitar</h4>
+          <div class="rating">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star-o"></i>
+          </div>
+          <p>$50.00</p>
+          <a href="" class="btn">Details &#8594;</a>
+        </div>
+        <div class="col-4">
+          <img src="images/product-3.jpeg" alt="" />
+          <h4>Abstract guitar</h4>
+          <div class="rating">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star-o"></i>
+          </div>
+          <p>$50.00</p>
+          <a href="" class="btn">Details &#8594;</a>
+        </div>
+        <div class="col-4">
+          <img src="images/product-4.jpeg" alt="" />
+          <h4>Abstract guitar</h4>
+          <div class="rating">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star-o"></i>
+          </div>
+          <p>$50.00</p>
+          <a href="" class="btn">Details &#8594;</a>
+        </div>
+      </div>
+    </div>
+
+    <!-- ====  testimonial ==== -->
+    <div class="testimonial">
+      <div class="small-container">
+        <div class="row">
+          <div class="col-3">
+            <i class="fa fa-quote-left"></i>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor,
+              odio vero! Animi ea accusamus non, recusandae aperiam, architecto,
+              hic magnam laborum maxime corporis sequi alias vero nesciunt
+              numquam nemo nam!
+            </p>
+            <div class="rating">
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star-o"></i>
+            </div>
+            <img src="images/user-1.jpeg" alt="user-1" />
+            <h3>kemi msabeni</h3>
+          </div>
+          <div class="col-3">
+            <i class="fa fa-quote-left"></i>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor,
+              odio vero! Animi ea accusamus non, recusandae aperiam, architecto,
+              hic magnam laborum maxime corporis sequi alias vero nesciunt
+              numquam nemo nam!
+            </p>
+            <div class="rating">
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star-o"></i>
+            </div>
+            <img src="images/user-1.jpeg" alt="user-1" />
+            <h3>kemi msabeni</h3>
+          </div>
+          <div class="col-3">
+            <i class="fa fa-quote-left"></i>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor,
+              odio vero! Animi ea accusamus non, recusandae aperiam, architecto,
+              hic magnam laborum maxime corporis sequi alias vero nesciunt
+              numquam nemo nam!
+            </p>
+            <div class="rating">
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star-o"></i>
+            </div>
+            <img src="images/user-1.jpeg" alt="user-1" />
+            <h3>kemi msabeni</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- ==== footer ==== -->
+    <div class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="footer-col-1">
+            <h3>dont know yet</h3>
+            <div class="feather-logo">
+              <img src="images/feather-logo_ccexpress.png" alt="" />
+            </div>
+            <p>some text, some text, some text adipisicing.</p>
+          </div>
+          <div class="footer-col-2">
+            <img src="images/art_logo2_ccexpress.png" alt="logo" />
+            <p>
+              my purpose is to sell you beautiful art pieces that will transform
+              the aesthetic of your spaces
+            </p>
+          </div>
+          <!-- <div class="footer-col-3">
+            <h3>quick links</h3>
+            <ul>
+              <li>link</li>
+              <li>link</li>
+              <li>link</li>
+              <li>link</li>
+            </ul>
+          </div> -->
+          <div class="footer-col-3">
+            <h3>follow me</h3>
+            <ul>
+              <li>Facebook</li>
+              <li>Instagram</li>
+              <li>Twitter</li>
+              <li>Youtube</li>
+            </ul>
+          </div>
+        </div>
+        <hr />
+        <p class="copyright">copyright 2022- kemi</p>
+      </div>
+    </div>
+
+
+    <!-- details modal -->
+    <?php include 'details-modal-product-1.php';
+          include 'details-modal-product-2.php';
+          include 'details-modal-product-3.php';
+          include 'details-modal-product-4.php';
+    ?>
+  </body>
+</html>
